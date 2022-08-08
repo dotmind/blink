a03c23991e8d36e798a8852956acb285d143fd3a
Valentin Magry <valou32720@gmail.com>
2022-08-08 11:09:02 +0200

    [FINGERPRINT] Create session service file, with fingerprint management

---
+++ src/app/services/session.ts
@@ -0,0 +1,7 @@
+import { load } from '@fingerprintjs/fingerprintjs';
+
+export async function createFingerprint() {
+  const fp = await load();
+  const { visitorId } = await fp.get();
+  return visitorId;
+}
