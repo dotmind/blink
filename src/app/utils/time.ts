b2fa016eaf004aca08638da4715bf579102e34d0
Valentin Magry <valou32720@gmail.com>
2022-08-17 14:33:00 +0200

    [FIX] Use round instead of floor for days, month & years

--- src/app/utils/time.ts
+++ src/app/utils/time.ts
@@ -6,9 +6,9 @@ export function timeRemaining(expiresAt: string): string {
   const seconds = Math.floor(diff / 1000);
   const minutes = Math.floor(seconds / 60);
   const hours = Math.floor(minutes / 60);
-  const days = Math.floor(hours / 24);
-  const months = Math.floor(days / 30);
-  const years = Math.floor(months / 12);
+  const days = Math.round(hours / 24);
+  const months = Math.round(days / 30);
+  const years = Math.round(months / 12);
 
   if (seconds < 60) {
     return `${seconds} seconds`;
