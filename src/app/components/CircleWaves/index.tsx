bf47e1c2f5f295bd57a4ab16ee3c3b0d283ff168
Valentin Magry <valou32720@gmail.com>
2022-08-22 09:33:24 +0200

    [ANIM] Enhance circle waves animation

--- src/app/components/CircleWaves/index.tsx
+++ src/app/components/CircleWaves/index.tsx
@@ -1,9 +1,30 @@
+import { useMemo } from 'react';
+import classNames from 'classnames';
+
+import { useUpload, UploadStatus } from '@/modules/upload/providers/UploadProvider';
+
 import styles from './styles.module.scss';
 
 function CircleWaves() {
+  const { status } = useUpload();
+
+  const css = useMemo(() => {
+    switch (status) {
+      case UploadStatus.UPLOADING:
+        return classNames(styles.circle_waves_base, styles.uploading);
+      case UploadStatus.SUCCESS:
+        return classNames(styles.circle_waves_base, styles.success);
+      case UploadStatus.IDLE:
+      default:
+        return styles.circle_waves_base;
+    }
+  }, [status]);
+
   return (
     <div className={styles.circle_container}>
-      <div className={styles.circle_waves_base}>
+      <div className={css}>
+        <div className={styles.animated_circles} />
+        <div className={styles.animated_circles} />
         <div className={styles.animated_circles} />
         <div className={styles.animated_circles} />
         <div className={styles.animated_circles} />
