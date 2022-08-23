e91a599c5f4ac952af6431705bd73d560dc9f4c1
Valentin Magry <valou32720@gmail.com>
2022-08-23 16:52:26 +0200

    [FEAT] Can switch between ModalFrame

--- src/app/components/About/index.tsx
+++ src/app/components/About/index.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react';
+import { useState, useMemo, useCallback } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
@@ -14,28 +14,51 @@ import styles from '@/app/components/About/styles.module.scss';
 function About() {
   const { width } = useWindowSize();
   const isMobile = useMemo(() => width < 768, [width]);
+  const [frameIndex, setFrameIndex] = useState(0);
+
+  // @TODO: find a way to add opacity animation on switch
+  const handleNext = useCallback(() => {
+    if (frameIndex < 1) {
+      setFrameIndex(frameIndex + 1);
+    }
+  }, [frameIndex]);
+
+  const handlePrevious = useCallback(() => {
+    if (frameIndex > 0) {
+      setFrameIndex(frameIndex - 1);
+    }
+  }, [frameIndex]);
+
+  const canPrevious = useMemo(() => frameIndex > 0, [frameIndex]);
+  const canNext = useMemo(() => frameIndex < 1, [frameIndex]);
 
   return isMobile ? (
     <Modal>
       <div className={styles.modal_content}>
-        <ModalFrame title={'No Shit, un outil libre et securiser pour partager vos fichiers'} image={background}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut arcu orci sed tristique enim quis tristique eu. Pellentesque
-          nisi, viverra aliquet quisque enim posuere aliquam augue. Congue at senectus sit sagittis varius nullam scelerisque
-          tortor. Sit sapien in ac vel dolor vestibulum. Ultricies purus faucibus imperdiet consectetur pulvinar a. Eu.
-        </ModalFrame>
+        {frameIndex === 0 && (
+          <ModalFrame title={'No Shit, un outil libre et securiser pour partager vos fichiers'} image={background}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut arcu orci sed tristique enim quis tristique eu.
+            Pellentesque nisi, viverra aliquet quisque enim posuere aliquam augue. Congue at senectus sit sagittis varius nullam
+            scelerisque tortor. Sit sapien in ac vel dolor vestibulum. Ultricies purus faucibus imperdiet consectetur pulvinar a.
+            Eu.
+          </ModalFrame>
+        )}
 
-        <ModalFrame title={'Une securité haut niveau grâce au E2E'} image={background_2}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut arcu orci sed tristique enim quis tristique eu. Pellentesque
-          nisi, viverra aliquet quisque enim posuere aliquam augue. Congue at senectus sit sagittis varius nullam scelerisque
-          tortor. Sit sapien in ac vel dolor vestibulum. Ultricies purus faucibus imperdiet consectetur pulvinar a. Eu.
-        </ModalFrame>
+        {frameIndex === 1 && (
+          <ModalFrame title={'Une securité haut niveau grâce au E2E'} image={background_2}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut arcu orci sed tristique enim quis tristique eu.
+            Pellentesque nisi, viverra aliquet quisque enim posuere aliquam augue. Congue at senectus sit sagittis varius nullam
+            scelerisque tortor. Sit sapien in ac vel dolor vestibulum. Ultricies purus faucibus imperdiet consectetur pulvinar a.
+            Eu.
+          </ModalFrame>
+        )}
 
         <div className={styles.frame_controls}>
-          <Button style={ButtonStyle.WHITE} callback={() => {}}>
+          <Button style={ButtonStyle.WHITE} callback={handleNext} disabled={!canNext}>
             Page suivante <FontAwesomeIcon icon={faArrowRight} />
           </Button>
 
-          <Button style={ButtonStyle.WHITE} callback={() => {}} disabled>
+          <Button style={ButtonStyle.WHITE} callback={handlePrevious} disabled={!canPrevious}>
             <FontAwesomeIcon icon={faArrowLeft} />
           </Button>
         </div>
