926bebe8c4ff5df86b9c37e23706a097cbe09d03
Valentin Magry <valou32720@gmail.com>
2022-08-24 10:53:02 +0200

    [UI] Fix loader components UI

--- src/app/components/Loader.tsx
+++ src/app/components/Loader.tsx
@@ -3,7 +3,11 @@ import { Player } from '@lottiefiles/react-lottie-player';
 import animation from '@/app/assets/lottie/loader.json';
 
 function Loader() {
-  return <Player autoplay loop src={animation} style={{ width: '64px', height: '64px' }} />;
+  return (
+    <div className={'d-flex justify-center align-center'} style={{ width: '100vw', height: '100vh' }}>
+      <Player autoplay loop src={animation} style={{ width: '128px', height: '128px' }} />
+    </div>
+  );
 }
 
 export default Loader;
