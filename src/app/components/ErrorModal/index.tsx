c838575dd784bb19bab12f9a060be3eca73074bd
Valentin Magry <valou32720@gmail.com>
2022-08-25 11:03:07 +0200

    [UI] Create error modal components & mobile UI attached

---
+++ src/app/components/ErrorModal/index.tsx
@@ -0,0 +1,60 @@
+import { useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRotateRight } from '@fortawesome/free-solid-svg-icons';
+
+import { useUpload, UploadStatus } from '@/modules/upload/providers/UploadProvider';
+import useIsMobile from '@/app/hooks/useIsMobile';
+import Button, { ButtonStyle } from '@/app/components/Button';
+import errorSVG from '@/app/assets/svg/error.svg';
+
+import styles from '@/app/components/ErrorModal/styles.module.scss';
+
+function ErrorModal() {
+  const { t } = useTranslation();
+  const isMobile = useIsMobile();
+  const { setStatus } = useUpload();
+
+  const handleRetry = () => {
+    setStatus(UploadStatus.IDLE);
+    // @TODO: Retry upload
+  };
+
+  const handleCancel = () => {
+    setStatus(UploadStatus.IDLE);
+  };
+
+  const renderMobile = useMemo(
+    () => (
+      <div className={styles.modal_mobile}>
+        <div className={styles.backdrop} />
+
+        <div className={styles.modal_content}>
+          <img src={errorSVG} alt={'error icon'} />
+
+          <div>
+            <h4>{t('upload.errors.upload_failed.title')}</h4>
+            <p>{t('upload.errors.upload_failed.description')}</p>
+          </div>
+
+          <div className={styles.controls}>
+            <Button callback={handleRetry} style={ButtonStyle.PRIMARY}>
+              {t('upload.errors.upload_failed.retry')}
+              <FontAwesomeIcon icon={faRotateRight} />
+            </Button>
+            <Button callback={handleCancel} style={ButtonStyle.WHITE}>
+              {t('upload.errors.upload_failed.cancel')}
+            </Button>
+          </div>
+        </div>
+      </div>
+    ),
+    [isMobile],
+  );
+
+  const renderDesktop = useMemo(() => <div>desktop</div>, [isMobile]);
+
+  return isMobile ? renderMobile : renderDesktop;
+}
+
+export default ErrorModal;
