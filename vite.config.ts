020e7e9020d427970486d224c8f96c5afa1e93cb
Valentin Magry <valou32720@gmail.com>
2022-08-29 11:08:27 +0200

    [CONFIG] Enable https for localhost server

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig, PluginOption } from 'vite';
 import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
 import { resolve } from 'path';
+import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // @xxx Vite issue https://github.com/btd/rollup-plugin-visualizer/issues/124#issuecomment-1221295477
-  plugins: [react(), visualizer() as PluginOption],
+  plugins: [react(), visualizer() as PluginOption, mkcert()],
+  server: { https: true, port: 8080 },
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
