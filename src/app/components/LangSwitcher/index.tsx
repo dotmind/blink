5a502a83e7b37030bbca8bc1f7973e804a3fb496
Valentin Magry <valou32720@gmail.com>
2022-09-01 10:13:51 +0200

    [FEAT] Dropdown for langswitcher

--- src/app/components/LangSwitcher/index.tsx
+++ src/app/components/LangSwitcher/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLanguage } from '@fortawesome/free-solid-svg-icons';
@@ -8,26 +9,28 @@ import styles from '@/app/components/LangSwitcher/styles.module.scss';
 
 function LangSwitcher() {
   const { t, i18n } = useTranslation();
+  const [isOpen, setIsOpen] = useState(false);
 
-  // const handleSwitchLanguage = useCallback(() => {
-  //   const currentLanguage = availableLanguages.indexOf(i18n.language);
-  //   const nextLanguage = (currentLanguage + 1) % availableLanguages.length;
-  //   i18n.changeLanguage(availableLanguages[nextLanguage]);
-  // }, [i18n]);
+  const handleSwitchLanguage = (lang: string) => {
+    i18n.changeLanguage(lang);
+    setIsOpen(false);
+  };
 
-  const openDropdown = () => {};
+  const openDropdown = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
-    <div>
+    <div className={styles.dropdown_container}>
       <button className={styles.langSwitcher} type={'button'} onClick={openDropdown}>
         <p className={styles.lang}>{t(`lang.${i18n.language}`)}</p>
         <FontAwesomeIcon icon={faLanguage} />
       </button>
-      <div className={styles.lang_dropdown}>
+      <div className={styles.lang_dropdown} data-isopen={isOpen}>
         {availableLanguages
           .filter((lang) => lang !== i18n.language)
           .map((lang) => (
-            <button key={lang} type={'button'} onClick={() => i18n.changeLanguage(lang)}>
+            <button key={lang} type={'button'} onClick={() => handleSwitchLanguage(lang)}>
               {t(`lang.${lang}`)}
             </button>
           ))}
