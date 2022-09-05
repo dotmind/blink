8c1169585f73193a2ed29ff51831b9b78437b468
Valentin Magry <valou32720@gmail.com>
2022-09-05 10:46:33 +0200

    [FEAT] Create Head components based on Helmet

---
+++ src/app/components/Head.tsx
@@ -0,0 +1,10 @@
+import { Helmet } from 'react-helmet';
+
+const index = () => (
+  <Helmet>
+    <title>Noshit</title>
+    <meta name={'description'} content={'Noshit application'} />
+  </Helmet>
+);
+
+export default index;
