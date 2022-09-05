19dd9e2c4b1e2aec058dbb29ec98ed1438ef01f6
Valentin Magry <valou32720@gmail.com>
2022-09-05 11:20:58 +0200

    [FEAT] Add opengraph meta

--- src/app/components/Head.tsx
+++ src/app/components/Head.tsx
@@ -1,9 +1,14 @@
 import { Helmet } from 'react-helmet';
 
+import placeholder from '@/app/assets/images/placeholder.png';
+
 const index = () => (
   <Helmet>
     <title>Noshit</title>
     <meta name={'description'} content={'Noshit application'} />
+    <meta property={'og:title'} content={'Noshit'} />
+    <meta property={'og:description'} content={'Noshit application'} />
+    <meta property={'og:image'} content={placeholder} />
   </Helmet>
 );
 
