d56a1d6df25fefd80cb6edb548b1026eb966019f
Valentin Magry <valou32720@gmail.com>
2022-09-05 11:23:40 +0200

    [FIX] Renaiming Head component

--- src/app/components/Head.tsx
+++ src/app/components/Head.tsx
@@ -2,14 +2,16 @@ import { Helmet } from 'react-helmet';
 
 import placeholder from '@/app/assets/images/placeholder.png';
 
-const index = () => (
-  <Helmet>
-    <title>Noshit</title>
-    <meta name={'description'} content={'Noshit application'} />
-    <meta property={'og:title'} content={'Noshit'} />
-    <meta property={'og:description'} content={'Noshit application'} />
-    <meta property={'og:image'} content={placeholder} />
-  </Helmet>
-);
+function Head() {
+  return (
+    <Helmet>
+      <title>Noshit</title>
+      <meta name={'description'} content={'Noshit application'} />
+      <meta property={'og:title'} content={'Noshit'} />
+      <meta property={'og:description'} content={'Noshit application'} />
+      <meta property={'og:image'} content={placeholder} />
+    </Helmet>
+  );
+}
 
-export default index;
+export default Head;
