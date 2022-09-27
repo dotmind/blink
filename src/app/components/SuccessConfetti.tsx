c76737059937830525dfb9b4d497d0a7726e2561
Valentin Magry <valou32720@gmail.com>
2022-09-27 15:16:58 +0200

    [FEAT] Add confetti animation on succes upload

---
+++ src/app/components/SuccessConfetti.tsx
@@ -0,0 +1,13 @@
+import { Player } from '@lottiefiles/react-lottie-player';
+
+import animation from '@/app/assets/lottie/success-confetti.json';
+
+function SuccessConfetti() {
+  return (
+    <div className={'success-confetti'}>
+      <Player keepLastFrame autoplay src={animation} style={{ width: '404px', height: '404px' }} />
+    </div>
+  );
+}
+
+export default SuccessConfetti;
