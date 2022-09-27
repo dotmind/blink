07b00827d60bc75b1627f09297eff09e544fe047
Valentin Magry <valou32720@gmail.com>
2022-09-27 10:07:30 +0200

    [FEAT] Create FileHead component to manage custom head with file's data

---
+++ src/modules/download/components/FileHead.tsx
@@ -0,0 +1,18 @@
+import { Helmet } from 'react-helmet-async';
+import { useTranslation } from 'react-i18next';
+
+import { useDownload } from '@/modules/download/providers/DownloadProvider';
+
+function FileHead() {
+  const { fileName } = useDownload();
+  const { t } = useTranslation();
+
+  return (
+    <Helmet>
+      <title>{`Noshit - ${fileName}`}</title>
+      <meta name={'description'} content={`${t('common.share.download')} ${fileName} - ${t('common.share.description')}`} />
+    </Helmet>
+  );
+}
+
+export default FileHead;
