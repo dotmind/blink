a83088435db7cc9d3eb1cbfbb258316833f820fd
Valentin Magry <valou32720@gmail.com>
2022-09-28 14:09:59 +0200

    [FIX] Add translation key for upload text content & remove compression refs

--- src/modules/upload/components/UploadHeader/index.tsx
+++ src/modules/upload/components/UploadHeader/index.tsx
@@ -1,28 +1,30 @@
 import { useMemo, useRef } from 'react';
+import { useTranslation } from 'react-i18next';
 
 import { useUpload, UploadStatus } from '@/modules/upload/providers/UploadProvider';
 
 import styles from '@/modules/upload/components/UploadHeader/styles.module.scss';
 
-function UploadHeader() {
+function UploadHeader(): JSX.Element {
   const { status } = useUpload();
   const container = useRef(null);
+  const { t } = useTranslation();
 
-  const renderHeader = useMemo(() => {
+  const renderHeader: JSX.Element = useMemo(() => {
     switch (status) {
       case UploadStatus.UPLOADING:
         return (
           <>
-            <h1>Compression et envoi en cours...</h1>
-            <p>Les fichiers que vous partager sont protégés par en chiffrement E2E</p>
+            <h1>{t('upload.headers.uploading.title')}</h1>
+            <p>{t('upload.headers.uploading.description')}</p>
           </>
         );
 
       case UploadStatus.SUCCESS:
         return (
           <>
-            <h1>C’est terminé ! Bravo ✨</h1>
-            <p>Partager ce fichier représente 80.7 g de CO2, c’est -54% de moins que sur Google Drive 🌱</p>
+            <h1>{t('upload.headers.success.title')}</h1>
+            <p>{t('upload.headers.success.description')}</p>
           </>
         );
 
@@ -30,8 +32,8 @@ function UploadHeader() {
       default:
         return (
           <>
-            <h1>Déposer vos fichier ici...</h1>
-            <p>Partager facilement des fichiers en toute sécurité.</p>
+            <h1>{t('upload.headers.idle.title')}</h1>
+            <p>{t('upload.headers.idle.description')}</p>
           </>
         );
     }
