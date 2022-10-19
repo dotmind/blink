352f1203755dee5c147098a9479beb6d2be30087
Valentin Magry <valou32720@gmail.com>
2022-10-19 11:32:47 +0200

    [UI] Fix tab tracker wrong position on init & update position on screen resize

--- src/app/components/TabSelector/index.tsx
+++ src/app/components/TabSelector/index.tsx
@@ -1,6 +1,8 @@
 import { useState, useRef, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 
+import useWindowSize from '@/app/hooks/useWindowSize';
+
 import styles from '@/app/components/TabSelector/styles.module.scss';
 
 interface IProps {
@@ -11,20 +13,23 @@ interface IProps {
 function TabSelector({ children, options }: IProps): JSX.Element {
   const [selected, setSelected] = useState<number>(1);
   const { t } = useTranslation();
+  const { width } = useWindowSize();
 
   const toggles = useRef<HTMLButtonElement[]>([]);
   const tabsTracker = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    const tracker = tabsTracker.current;
-    const currentTab = toggles.current[selected];
+    setTimeout(() => {
+      const tracker: HTMLElement | null = tabsTracker.current;
+      const currentTab: HTMLElement = toggles.current[selected];
 
-    if (tracker && currentTab) {
-      tracker.style.left = `${currentTab.offsetLeft}px`;
-      tracker.style.width = `${currentTab.offsetWidth}px`;
-      tracker.style.height = `${currentTab.offsetHeight}px`;
-    }
-  }, [selected]);
+      if (tracker && currentTab) {
+        tracker.style.left = `${currentTab.offsetLeft}px`;
+        tracker.style.width = `${currentTab.offsetWidth}px`;
+        tracker.style.height = `${currentTab.offsetHeight}px`;
+      }
+    }, 25);
+  }, [selected, width]);
 
   return (
     <>
