045584b849806e3574d1a97301b5b07dbd092358
Valentin Magry <valou32720@gmail.com>
2022-10-25 16:21:55 +0200

    [FIX] Add safe zone around title

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -25,8 +25,11 @@ function Home() {
           {isMobile && <LangSwitcher />}
         </header>
         <div className={'d-flex flex-column align-center grow'}>
+          <div className={'safe text-center'}>
+
           <h1 className={'title'}> {t('home.title')} </h1>
           <p className={'subtitle'}> {t('home.subtitle')} </p>
+          </div>
           <TabSelector options={['home.tabs.history', 'home.tabs.upload', 'home.tabs.about']}>
             <History />
             <Upload />
