afa839ae78857d42b6d20f5eb114b5a2e74d0a13
Valentin Magry <valou32720@gmail.com>
2022-11-02 09:48:30 +0100

    [CLEAN] Rename ShareButton to NewUploadButton

--- src/modules/upload/components/NewUploadButton/index.tsx
+++ src/modules/upload/components/NewUploadButton/index.tsx
@@ -2,16 +2,11 @@ import { useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import Button, { ButtonStyle } from '@/app/components/Button';
-import { useUpload } from '@/modules/upload/providers/UploadProvider';
-import { canUseNativeShare, nativeShare } from '@/app/services/navigator';
-import ShareIcon from '@/app/assets/svg/share_white.svg';
 
 import styles from '@/modules/upload/components/ShareButtons/styles.module.scss';
 
 function ShareButtons(): JSX.Element {
-  const { shareUrl } = useUpload();
   const { t } = useTranslation();
-  const handleShare: () => Promise<void> = useCallback(() => nativeShare(shareUrl as string), [shareUrl]);
 
   const handleReload = useCallback(() => {
     window.location.reload();
@@ -19,11 +14,6 @@ function ShareButtons(): JSX.Element {
 
   return (
     <div className={styles.share_container}>
-      {canUseNativeShare() && (
-        <Button style={ButtonStyle.SECONDARY} callback={handleShare} name={t('upload.link.share')}>
-          {t('upload.link.share')} <img className={'icons'} src={ShareIcon} alt={t('upload.link.share')} />
-        </Button>
-      )}
       <Button style={ButtonStyle.GRADIENT} callback={handleReload} name={t('upload.link.new_upload')}>
         {t('upload.link.new_upload')}
       </Button>
