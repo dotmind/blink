43230b547de6be6370ee88f589a7225651febce9
Valentin Magry <valou32720@gmail.com>
2022-11-21 10:48:24 +0100

    [FIX] Change preview loader only for PDF

--- src/app/components/Loader.tsx
+++ src/app/components/Loader.tsx
@@ -4,7 +4,7 @@ import animation from '@/app/assets/lottie/loader.json';
 
 function Loader(): JSX.Element {
   return (
-    <div className={'d-flex justify-center align-center'} style={{ width: '100vw', height: '100vh' }}>
+    <div className={'d-flex justify-center align-center grow'}>
       <Player autoplay loop src={animation} style={{ width: '128px', height: '128px' }} />
     </div>
   );
