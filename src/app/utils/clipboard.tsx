bca1e190f943a551c47e64b01696ad980853d90a
Mattèo Gauthier <matteo.gauthier@gmail.com>
2022-12-05 15:03:09 +0100

    [CLIPBOARD] Small padding issue caused by default img style

--- src/app/utils/clipboard.tsx
+++ src/app/utils/clipboard.tsx
@@ -24,7 +24,7 @@ function RichTextTemplate({ fileName, link }: RichTextTemplateProps) {
       href={link}>
       <img
         width={'240'}
-        style={{ borderRadius: '8px 8px 0px 0px', fontSize: 0 }}
+        style={{ borderRadius: '8px 8px 0px 0px', fontSize: 0, display: 'block' }}
         src={'https://inablink.io/assets/quick-link-preview.jpg'}
         alt={'Your document preview button'}
       />
